f6bbc61e97b54d9278c911c8811f8db8af82784a
Aditya Jain <adi.jain059@gmail.com>
2021-04-01 10:22:50 +0530

    Right Turn and Left Turn + Direction working properly

--- search_n_rescue/src/eBot_Sandbox.cpp
+++ search_n_rescue/src/eBot_Sandbox.cpp
@@ -13,7 +13,6 @@
 #include <stdlib.h>
 
 
-
 //------------------------------ GLOBAL VARIABLES -------------------------------
 
 // To store 8-bit data of left, center and right white line sensors
@@ -65,7 +64,8 @@ typedef struct
 {
 	int x, y;
 }tuple;
-tuple curr_loc = {4,0}, goal_loc = {4,5};
+
+tuple curr_loc = {0,4}, med_loc = {5,9};
 
 // To store the direction in which eBot is currently facing
 char dir_flag = 'n';
@@ -104,11 +104,11 @@ void forward_wls(unsigned char node)
 
 			readSensor();
 
-			if(left_wl_sensor_data < 50 && center_wl_sensor_data > 200 && right_wl_sensor_data < 50) { // WBW & BWB
+			if(left_wl_sensor_data < 50 && center_wl_sensor_data > 200 && right_wl_sensor_data < 50) { // WBW
 				forward();
 				velocity(200,200);
 			}
-			if(left_wl_sensor_data < 50 && right_wl_sensor_data > 200) {     // WWB & WBB
+			else if(left_wl_sensor_data < 50 && right_wl_sensor_data > 200) {     // WWB & WBB
 				right();
 				velocity(10,10);
 			}
@@ -118,17 +118,19 @@ void forward_wls(unsigned char node)
 			}
 			else if(left_wl_sensor_data > 200 && center_wl_sensor_data > 200 && right_wl_sensor_data > 200) {  // BBB
 				printf("Intersection Reached %d\n", i+1);
+				forward();
 				velocity(200,200);
-				_delay_ms(130);
+				_delay_ms(110);
 				stop();
-				if(dir_flag == 'n') curr_loc.y++;
-				else if(dir_flag == 's') curr_loc.y--;
-				else if(dir_flag == 'w') curr_loc.x--;
-				else curr_loc.x++;
-				printf("Curr.y: %d\nGoal.y: %d\n", curr_loc.y, goal_loc.y);
+				if(dir_flag == 'n') curr_loc.x++;
+				else if(dir_flag == 's') curr_loc.x--;
+				else if(dir_flag == 'w') curr_loc.y--;
+				else curr_loc.y++;
+				printf("Current: (%d, %d)\n", curr_loc.x, curr_loc.y);
+				printf("Goal: (%d, %d)\n", med_loc.x, med_loc.y);
 				break;
 			}
-			else if(left_wl_sensor_data < 50 && center_wl_sensor_data < 50 && right_wl_sensor_data < 50){  // WWW
+			else /*if(left_wl_sensor_data < 50 && center_wl_sensor_data < 50 && right_wl_sensor_data < 50)*/{  // WWW
 				if(t % 4 == 0){
 					left();
 					velocity(10,10);
@@ -169,9 +171,10 @@ void forward_wls(unsigned char node)
 */
 void left_turn_wls(void)
 {
+	printf("Left Turn\n");
 	int timer = 0;
 	left();
-	//_delay_ms(100);
+	_delay_ms(100);
 	velocity(20,20);
 	while(1) {
 		timer++;
@@ -184,10 +187,14 @@ void left_turn_wls(void)
 		_delay_ms(10);
 	}
 	printf("timer = %d\n", timer);
-	if(dir_flag == 'n') dir_flag = 'w';
-	else if(dir_flag == 'w') dir_flag = 's';
-	else if(dir_flag == 's') dir_flag = 'e';
-	else dir_flag = 'n';
+	for(int i = 0; i <= timer / 180; i++) {
+		if(dir_flag == 'n') dir_flag = 'w';
+		else if(dir_flag == 'w') dir_flag = 's';
+		else if(dir_flag == 's') dir_flag = 'e';
+		else dir_flag = 'n';
+		printf("Direction = %c\n", dir_flag);
+	}
+
 }
 
 
@@ -202,9 +209,10 @@ void left_turn_wls(void)
 */
 void right_turn_wls(void)
 {
+	printf("Right Turn\n");
 	int timer = 0;
 	right();
-	//_delay_ms(100);
+	_delay_ms(100);
 	velocity(20,20);
 	while(1) {
 		timer++;
@@ -216,11 +224,15 @@ void right_turn_wls(void)
 		}
 		_delay_ms(10);
 	}
-	if(dir_flag == 'n') dir_flag = 'e';
-	else if(dir_flag == 'w') dir_flag = 'n';
-	else if(dir_flag == 's') dir_flag = 'w';
-	else dir_flag = 's';
 	printf("Timer = %d\n", timer);
+	for(int i = 0; i <= timer / 180; i++) {
+		if(dir_flag == 'n') dir_flag = 'e';
+		else if(dir_flag == 'w') dir_flag = 'n';
+		else if(dir_flag == 's') dir_flag = 'w';
+		else dir_flag = 's';
+		printf("Direction = %c\n", dir_flag);
+	}
+
 }
 
 /**
@@ -335,19 +347,101 @@ void traverse_line_to_goal(void)
 		}
 	}
 */
-	forward_wls(1);
-
+	forward_wls(5);
 	left_turn_wls();
-
-
 	forward_wls(2);
 	right_turn_wls();
+	forward_wls(4);
+	left_turn_wls();
+	forward_wls(2);
+	left_turn_wls();
+	_delay_ms(1000);
+	//forward_wls(2);
+	right_turn_wls();
+	forward_wls(1);
 	_delay_ms(1000000);
-//	forward_wls(4);
+
+
+	//forward_wls(4);
+
 //	forward_wls(1);
 //	right_turn_wls();
 //	forward_wls(1);
 //	left_turn_wls();
 //	forward_wls(1);
 }
+
+
+
+
+tuple get_cords(unsigned char plot_no){
+	unsigned char plots_x[] = {8,6,4,2}, plots_y[] = {7,1,3,5};
+	tuple cords;
+	int x;
+	x = (plot_no - 1)/4;
+	cords.x = plots_x[x];
+	cords.y = plots_y[(plot_no % 4)];
+	return cords;
+}
+
+
+
+void traverse(void) {
+	tuple cords;
+	for(int i = 1; i <= 16; i++) {
+		cords = get_cords(i);
+		printf("Plot %d : (%d, %d)\n", i, cords.x, cords.y);
+		printf("Left Cords: (%d, %d)\n", cords.x, cords.y - 1);
+		printf("Right Cords: (%d, %d)\n", cords.x, cords.y + 1);
+		printf("Top Cords: (%d, %d)\n", cords.x + 1, cords.y);
+		printf("Bottom Cords: (%d, %d)\n\n", cords.x - 1, cords.y);
+		tuple leftCords = {cords.x, cords.y - 1};
+		tuple rightCords = {cords.x, cords.y + 1};
+		tuple topCords = {cords.x + 1, cords.y};
+		tuple bottomCords = {cords.x - 1, cords.y};
+
+		int distToLeftCord = (curr_loc.x - leftCords.x) + (curr_loc.y - leftCords.y);
+		int distToRightCord = (curr_loc.x - rightCords.x) + (curr_loc.y -rightCords.y);
+		int distToTopCord = (curr_loc.x - topCords.x) + (curr_loc.y - topCords.y);
+		int distToBottomCord = (curr_loc.x - bottomCords.x) + (curr_loc.y - bottomCords.y);
+
+		int minDist1, minDist2, maxDist1, maxDist2;
+		tuple minCord1, minCord2, maxCord1, maxCord2;
+		char minNode1, minNode2, maxNode1, maxNode2;
+		if(distToLeftCord <= distToRightCord) {
+			minDist1 = distToLeftCord;
+			minCord1 = leftCords;
+			minNode1 = 'L';
+			maxDist1 = distToRightCord;
+			maxCord1 = rightCords;
+			maxNode1 = 'R';
+		}
+		else {
+			minDist1 = distToRightCord;
+			minCord1 = rightCords;
+			minNode1 = 'R';
+			maxDist1 = distToLeftCord;
+			maxCord1 = leftCords;
+			maxNode1 = 'L';
+		}
+
+		if(distToTopCord <= distToBottomCord) {
+			minDist2 = distToTopCord;
+			minCord2 = topCords;
+			minNode2 = 'T';
+			maxDist2 = distToBottomCord;
+			maxCord2 = bottomCords;
+			maxNode2 = 'B';
+		}
+		else {
+			minDist2 = distToBottomCord;
+			minCord2 = bottomCords;
+			minNode2 = 'B';
+			maxDist2 = distToTopCord;
+			maxCord2 = topCords;
+			maxNode2 = 'T';
+		}
+	}
+}
+
 
